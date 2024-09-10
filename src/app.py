8e1b8a5a0c683279086f7fd6506292c3991ed38a
AbhishekBhargav <pullebhargav.4zeracing@gmail.com>
2024-09-11 00:10:51 +0530

    updated model

--- src/app.py
+++ src/app.py
@@ -1,15 +1,25 @@
-import streamlit as st 
+import streamlit as st
+import pickle
+
+
+with open("model.pkl","rb") as f:
+    loaded=pickle.load(f)
+
+model=loaded[0]
+target_names=loaded[1]
+sl_min,sl_max,sw_min,sw_max,pl_min,pl_max,pw_min,pw_max = loaded[2]
 
 st.title("Iris Flower species prediction")
 
-sl = st.slider("Sepal length(cm)",min_value=4.0,max_value=8.0,value=5.0,step=0.1)
-pl = st.slider("Petal length(cm)",min_value=4.0,max_value=8.0,value=5.0,step=0.1)
-sw = st.slider("Sepal Width(cm)",min_value=4.0,max_value=8.0,value=5.0,step=0.1)
-pw = st.slider("Petal width(cm)",min_value=4.0,max_value=8.0,value=5.0,step=0.1)
+sl = st.slider("Sepal length(cm)",min_value=sl_min,max_value=sl_max,value=5.0,step=0.1)
+pl = st.slider("Petal length(cm)",min_value=pl_min,max_value=pl_max,value=5.0,step=0.1)
+sw = st.slider("Sepal Width(cm)",min_value=sw_min,max_value=sw_max,value=5.0,step=0.1)
+pw = st.slider("Petal width(cm)",min_value=pw_min,max_value=pw_max,value=5.0,step=0.1)
 
 if st.button('Predict'):
     input_data = [[sl,sw,pl,pw]]
     # prediction = model.predict(input_data)
-    prediction = 'Versicolour'
+    # prediction = 'Versicolour'
+    prediction=model.predict(input_data)[0]
 
-    st.write(f"The predicted species is : **{prediction}**" )
+    st.write(f"The predicted species is : **{target_names[prediction]}**" )
--- train/model.py
+++ train/model.py
@@ -2,9 +2,23 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import pickle
+import numpy as np
 
 iris = load_iris()
+target_labels=iris.target_names
 X_train,X_test,y_train,y_test = train_test_split(iris.data,iris.target,test_size=0.2,random_state=42)
+
+sl_min=np.floor(X_train[:,0].min())
+sl_max=np.ceil(X_train[:,0].max())
+
+sw_min=np.floor(X_train[:,1].min())
+sw_max=np.ceil(X_train[:,1].max())
+
+pl_min=np.floor(X_train[:,2].min())
+pl_max=np.ceil(X_train[:,2].max())
+
+pw_min=np.floor(X_train[:,3].min())
+pw_max=np.ceil(X_train[:,3].max())
 
 model = RandomForestClassifier()
 model.fit(X_train,y_train)
@@ -13,4 +27,4 @@ accuracy = model.score(X_test,y_test)
 print(f"Model accuracy : {accuracy}")
 
 with open('../model.pkl','wb') as f:
-    pickle.dump(model,f)
+    pickle.dump([model,target_labels,[sl_min,sl_max,sw_min,sw_max,pl_min,pl_max,pw_min,pw_max]],f)
